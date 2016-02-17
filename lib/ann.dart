b2b2f6b69a9b574c5dee37d0f127c4453dfec412
Filip Hracek <filip.hracek@gmail.com>
2016-02-17 11:00:13 +0100

    Sort and clean up neural net code

--- lib/ann.dart
+++ lib/ann.dart
@@ -1,25 +1,41 @@
 import 'dart:math';
 import 'dart:typed_data';
 
-num _tanh(num x) => -1.0 + 2.0 / (1 + pow(E, (-2 * x)));
-
 Random _random = new Random();
 
-// A layer with Tan H neurons and two bias inputs (+1 and -1).
+/// A layer with Tan H neurons and two bias inputs (+1 and -1).
 class Layer {
   final int neuronCount;
   final int inputNeuronCount;
   Float32List _weights;
-  int get weightsPerNeuron => inputNeuronCount + 2 /* bias inputs */;
-  int get weightsTotal => neuronCount * weightsPerNeuron;
 
+  /// Creates a new neuron layer with [neuronCount] neurons which can connect
+  /// to a previous layer of [inputNeuronCount] neurons.
   Layer(this.neuronCount, int inputNeuronCount)
       : inputNeuronCount = inputNeuronCount {
     _weights = new Float32List(weightsTotal);
   }
 
-  List<double> pass(List<double> inputs) {
+  int get weightsPerNeuron => inputNeuronCount + 2 /* bias inputs */;
+
+  int get weightsTotal => neuronCount * weightsPerNeuron;
+
+  void randomizeWeights() {
+    for (int i = 0; i < _weights.length; i++) {
+      _weights[i] = _random.nextDouble() * 2 - 1;
+    }
+  }
+
+  void setWeights(List<num> weights) {
+    assert(weights.length == weightsTotal);
+    for (int i = 0; i < _weights.length; i++) {
+      _weights[i] = weights[i].toDouble();
+    }
+  }
+
+  List<double> use(List<double> inputs) {
     assert(inputs.length == inputNeuronCount);
+    assert(_weights.every((w) => w != null));
     Float32List outputs = new Float32List(neuronCount);
 
     for (int i = 0; i < neuronCount; i++) {
@@ -35,29 +51,21 @@ class Layer {
     return outputs;
   }
 
-  void randomizeWeights() {
-    for (int i = 0; i < _weights.length; i++) {
-      _weights[i] = _random.nextDouble() * 2 - 1;
-    }
-  }
-
-  void setWeights(List<num> weights) {
-    assert(weights.length == weightsTotal);
-    for (int i = 0; i < _weights.length; i++) {
-      _weights[i] = weights[i].toDouble();
-    }
-  }
+  num _tanh(num x) => -1.0 + 2.0 / (1 + pow(E, (-2 * x)));
 }
 
+/// Simple artificial neural network.
 class Network {
   final int inputs;
   final int outputs;
 
   List<Layer> _layers;
 
-  Network(inputs, outputs, {int hiddenLayers: 1})
-      : inputs = inputs,
-        outputs = outputs {
+  /// Creates a neural network.
+  ///
+  /// The synapse weights start uninitialized. Call [randomizeWeights()] or
+  /// [setWeights()] before using the network.
+  Network(this.inputs, this.outputs, {int hiddenLayers: 1}) {
     int hiddenLayerNeuronCount = (inputs + outputs) ~/ 2;
     _layers = new List<Layer>.generate(hiddenLayers + 1, (int index) {
       if (index == 0) {
@@ -70,9 +78,8 @@ class Network {
     });
   }
 
-  List<num> use(List<num> inputs) {
-    return _layers.fold(inputs, (prev, layer) => layer.pass(prev));
-  }
+  List<num> get weights => new List.unmodifiable(
+      _layers.map((layer) => layer._weights).expand((e) => e));
 
   void randomizeWeights() =>
       _layers.forEach((layer) => layer.randomizeWeights());
@@ -86,6 +93,7 @@ class Network {
     }
   }
 
-  List<num> get weights => new List.unmodifiable(
-      _layers.map((layer) => layer._weights).expand((e) => e));
+  List<num> use(List<num> inputs) {
+    return _layers.fold(inputs, (prev, layer) => layer.use(prev));
+  }
 }
